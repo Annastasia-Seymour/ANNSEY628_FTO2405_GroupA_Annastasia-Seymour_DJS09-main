01d4ae0e5438543d4e6e5ca9f7373b9dbcc1b440
Annastasia-Seymour <stasia1seymour@gmail.com>
2024-10-06 16:00:05 +0200

    fixed error ,missing tsconfig.json added

--- dist/index.pack.js
+++ dist/index.pack.js
@@ -7,6 +7,7 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./index.ts":
@@ -15,7 +16,7 @@
   \******************/
 /***/ (() => {
 
-eval("throw new Error(\"Module build failed (from ./node_modules/ts-loader/index.js):\\nError: \\u001b[31merror while parsing tsconfig.json\\u001b[39m\\n    at Object.loader (C:\\\\Users\\\\HP\\\\OneDrive\\\\Desktop\\\\CodeSpace\\\\Dynamic Java Script\\\\DJS09 Couch Surfing , Typescript Practice\\\\ANNSEY628_FTO2405_GroupA_Annastasia-Seymour_DJS09-main\\\\node_modules\\\\ts-loader\\\\dist\\\\index.js:18:18)\");\n\n//# sourceURL=webpack://project/./index.ts?");
+eval("\n// Number Types mini-challenge 10 10.2\n// Write a function that will only accept numbers and attend to \n// all TypeScript weakness flags.\n// : number\ndocument.addEventListener(\"DOMContentLoaded\", () => {\n    const returningUserDisplay = document.querySelector('#returning-user');\n    const userNameDisplay = document.querySelector('#user');\n    const reviewTotalDisplay = document.querySelector('#reviews');\n    const reviews = [\n        {\n            name: 'Sheia',\n            stars: 5,\n            loyaltyUser: true,\n            date: '01-04-2021'\n        },\n        {\n            name: 'Andrzej',\n            stars: 3,\n            loyaltyUser: false,\n            date: '28-03-2021'\n        },\n        {\n            name: 'Omar',\n            stars: 4,\n            loyaltyUser: true,\n            date: '27-03-2021'\n        },\n    ];\n    function showReviewTotal(value, reviewer, isLoyalty) {\n        const iconDisplay = isLoyalty ? '⭐' : '';\n        if (reviewTotalDisplay) {\n            reviewTotalDisplay.innerHTML = 'Review total ' + value.toString() + '| last reviewed by ' +\n                reviewer + ' ' + iconDisplay;\n        }\n    }\n    showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser);\n    const you = {\n        userName: 'Bobby',\n        isReturning: true,\n    };\n    function populateUser(isReturning, userName) {\n        if (isReturning && returningUserDisplay) {\n            returningUserDisplay.innerHTML = 'Welcome back';\n        }\n        if (userNameDisplay) {\n            userNameDisplay.innerHTML = userName;\n        }\n    }\n    populateUser(you.isReturning, you.userName);\n});\n\n\n//# sourceURL=webpack://project/./index.ts?");
 
 /***/ })
 
@@ -24,7 +25,7 @@ eval("throw new Error(\"Module build failed (from ./node_modules/ts-loader/index
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
-/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
 /******/ 	var __webpack_exports__ = {};
 /******/ 	__webpack_modules__["./index.ts"]();
 /******/ 	
