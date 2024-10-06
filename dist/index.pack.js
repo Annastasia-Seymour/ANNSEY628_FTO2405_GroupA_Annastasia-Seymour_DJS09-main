3777339f688a914e03da9c19f9bd0ec765ba70fe
Annastasia-Seymour <stasia1seymour@gmail.com>
2024-10-06 16:24:06 +0200

    Added object images

--- dist/index.pack.js
+++ dist/index.pack.js
@@ -14,20 +14,54 @@
 /*!******************!*\
   !*** ./index.ts ***!
   \******************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
 
-eval("\n// Number Types mini-challenge 10 10.2\n// Write a function that will only accept numbers and attend to \n// all TypeScript weakness flags.\n// : number\ndocument.addEventListener(\"DOMContentLoaded\", () => {\n    const returningUserDisplay = document.querySelector('#returning-user');\n    const userNameDisplay = document.querySelector('#user');\n    const reviewTotalDisplay = document.querySelector('#reviews');\n    let isOpen;\n    const reviews = [\n        {\n            name: 'Sheia',\n            stars: 5,\n            loyaltyUser: true,\n            date: '01-04-2021'\n        },\n        {\n            name: 'Andrzej',\n            stars: 3,\n            loyaltyUser: false,\n            date: '28-03-2021'\n        },\n        {\n            name: 'Omar',\n            stars: 4,\n            loyaltyUser: true,\n            date: '27-03-2021'\n        },\n    ];\n    function showReviewTotal(value, reviewer, isLoyalty) {\n        const iconDisplay = isLoyalty ? '⭐' : '';\n        reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer + ' ' + iconDisplay;\n    }\n    showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser);\n    const you = {\n        firstName: 'Bobby',\n        lastName: 'Brown',\n        isReturning: true,\n        age: 35,\n    };\n    function populateUser(isReturning, userName) {\n        if (isReturning) {\n            returningUserDisplay.innerHTML = 'back';\n        }\n        userNameDisplay.innerHTML = userName;\n    }\n    populateUser(you.isReturning, you.firstName);\n    //sorry coach Josh if the file was enormous i had to sort an issue with the compiler(i think?)\n});\n//sorry coach Josh if the file was enormous i had to sort an issue with the compiler(i think?)\n\n\n//# sourceURL=webpack://project/./index.ts?");
+eval("\n// Number Types mini-challenge 10 10.2\n// Write a function that will only accept numbers and attend to \n// all TypeScript weakness flags.\nObject.defineProperty(exports, \"__esModule\", ({ value: true }));\nconst propertyContainer = document.querySelector('.properties');\nconst utils_1 = __webpack_require__(/*! ./utils */ \"./utils.ts\");\nlet isOpen;\n// Reviews\nconst reviews = [\n    {\n        name: 'Sheia',\n        stars: 5,\n        loyaltyUser: true,\n        date: '01-04-2021'\n    },\n    {\n        name: 'Andrzej',\n        stars: 3,\n        loyaltyUser: false,\n        date: '28-03-2021'\n    },\n    {\n        name: 'Omar',\n        stars: 4,\n        loyaltyUser: true,\n        date: '27-03-2021'\n    },\n];\n// User\nconst you = {\n    firstName: 'Bobby',\n    lastName: 'Brown',\n    isReturning: true,\n    age: 35,\n    stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']\n};\n// Array of Properties\nconst properties = [\n    {\n        image: 'images/colombia-property.jpg',\n        title: 'Colombian Shack',\n        price: 45,\n        location: {\n            firstLine: 'shack 37',\n            city: 'Bogota',\n            code: 45632,\n            country: 'Colombia'\n        },\n        contact: 'marywinkle@gmail.com',\n        isAvailable: true\n    },\n    {\n        image: 'images/poland-property.jpg',\n        title: 'Polish Cottage',\n        price: 34,\n        location: {\n            firstLine: 'no 23',\n            city: 'Gdansk',\n            code: 343903,\n            country: 'Poland'\n        },\n        contact: 'garydavis@hotmail.com',\n        isAvailable: false\n    },\n    {\n        image: 'images/london-property.jpg',\n        title: 'London Flat',\n        price: 23,\n        location: {\n            firstLine: 'flat 15',\n            city: 'London',\n            code: 35433,\n            country: 'United Kingdom',\n        },\n        contact: 'andyluger@aol.com',\n        isAvailable: true\n    }\n];\n// Functions\n(0, utils_1.showReviewTotal)(reviews.length, reviews[0].name, reviews[0].loyaltyUser);\n(0, utils_1.populateUser)(you.isReturning, you.firstName);\n//Add the properties\nfor (let i = 0; i < properties.length; i++) {\n    const card = document.createElement('div');\n    card.classList.add('card');\n    card.innerHTML = properties[i].title;\n    const image = document.createElement('img');\n    image.setAttribute('src', properties[i].image);\n    card.appendChild(image);\n    propertyContainer.appendChild(card);\n}\n//sorry coach Josh if the file was enormous i had to sort an issue with the compiler(i think?)\n\n\n//# sourceURL=webpack://project/./index.ts?");
+
+/***/ }),
+
+/***/ "./utils.ts":
+/*!******************!*\
+  !*** ./utils.ts ***!
+  \******************/
+/***/ ((__unused_webpack_module, exports) => {
+
+eval("\nObject.defineProperty(exports, \"__esModule\", ({ value: true }));\nexports.showReviewTotal = showReviewTotal;\nexports.populateUser = populateUser;\nconst reviewTotalDisplay = document.querySelector('#reviews');\nconst returningUserDisplay = document.querySelector('#returning-user');\nconst userNameDisplay = document.querySelector('#user');\nfunction showReviewTotal(value, reviewer, isLoyalty) {\n    const iconDisplay = isLoyalty ? '⭐' : '';\n    reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer + ' ' + iconDisplay;\n}\nfunction populateUser(isReturning, userName) {\n    if (isReturning) {\n        returningUserDisplay.innerHTML = 'back';\n    }\n    userNameDisplay.innerHTML = userName;\n}\n\n\n//# sourceURL=webpack://project/./utils.ts?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./index.ts"]();
+/******/ 	var __webpack_exports__ = __webpack_require__("./index.ts");
 /******/ 	
 /******/ })()
 ;
--- index.ts
+++ index.ts
@@ -1,64 +1,128 @@
 // Number Types mini-challenge 10 10.2
 // Write a function that will only accept numbers and attend to 
 // all TypeScript weakness flags.
-// : number
-document.addEventListener("DOMContentLoaded", () => {
-    const returningUserDisplay = document.querySelector('#returning-user')
-const userNameDisplay = document.querySelector('#user')
-const reviewTotalDisplay = document.querySelector('#reviews')
+
+const propertyContainer = document.querySelector('.properties')
+
+import { showReviewTotal, populateUser } from './utils'
 let isOpen: boolean
 
-    const reviews = [
-        {
-            name: 'Sheia',
-            stars: 5,
-            loyaltyUser: true,
-            date: '01-04-2021'
-        },
-        {
-            name: 'Andrzej',
-            stars: 3,
-            loyaltyUser: false,
-            date: '28-03-2021'
-        },
-        {
-            name: 'Omar',
-            stars: 4,
-            loyaltyUser: true,
-            date: '27-03-2021'
-        },
-    ]
-    
+// Reviews
+const reviews : { 
+    name: string; 
+    stars: number; 
+    loyaltyUser: boolean; 
+    date: string
+    }[] = [
+    {
+        name: 'Sheia',
+        stars: 5,
+        loyaltyUser: true,
+        date: '01-04-2021'
+    },
+    {
+        name: 'Andrzej',
+        stars: 3,
+        loyaltyUser: false,
+        date: '28-03-2021'
+    },
+    {
+        name: 'Omar',
+        stars: 4,
+        loyaltyUser: true,
+        date: '27-03-2021'
+    },
+]
 
-    function showReviewTotal(value: number, reviewer: string, isLoyalty: boolean) {
-        const iconDisplay = isLoyalty ? '⭐' : ''
-        reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer + ' ' + iconDisplay
+// User
+const you: {
+    firstName: string;
+    lastName: string;
+    isReturning: boolean;
+    age: number;
+    stayedAt: string[]
+} = {
+    firstName: 'Bobby',
+    lastName: 'Brown',
+    isReturning: true,
+    age: 35,
+    stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']
+}
+
+// Array of Properties
+const properties : {
+    image: string;
+    title: string;
+    price: number;
+    location: {
+        firstLine: string;
+        city: string;
+        code: number;
+        country: string;
+    };
+    contact: string;
+    isAvailable: boolean;
+}[] = [
+    {
+        image: 'images/colombia-property.jpg',
+        title: 'Colombian Shack',
+        price: 45,
+        location: {
+            firstLine: 'shack 37',
+            city: 'Bogota',
+            code: 45632,
+            country: 'Colombia'
+        },
+        contact: 'marywinkle@gmail.com',
+        isAvailable: true  
+    },
+    {
+        image: 'images/poland-property.jpg',
+        title: 'Polish Cottage',
+        price: 34,
+        location: {
+            firstLine: 'no 23',
+            city: 'Gdansk',
+            code: 343903,
+            country: 'Poland'
+        },
+        contact: 'garydavis@hotmail.com',
+        isAvailable: false 
+    },
+    {
+        image: 'images/london-property.jpg',
+        title: 'London Flat',
+        price: 23,
+        location: {
+            firstLine: 'flat 15',
+            city: 'London',
+            code: 35433,
+            country: 'United Kingdom',
+        },
+        contact: 'andyluger@aol.com',
+        isAvailable: true
     }
-    
-    showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
-    
-    const you: {
-        firstName: string;
-        lastName: string;
-        isReturning: boolean;
-        age: number;
-    } = {
-        firstName: 'Bobby',
-        lastName: 'Brown',
-        isReturning: true,
-        age: 35,
-    }
-    
-    
-    function populateUser(isReturning : boolean, userName: string ) {
-        if (isReturning){
-            returningUserDisplay.innerHTML = 'back'
-        }
-        userNameDisplay.innerHTML = userName
-    }
-    
-    populateUser(you.isReturning, you.firstName)
-    
+]
+
+// Functions
+showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
+
+populateUser(you.isReturning, you.firstName)
+
+//Add the properties
+for (let i = 0; i < properties.length; i++) {
+    const card = document.createElement('div')
+    card.classList.add('card')
+    card.innerHTML = properties[i].title
+    const image = document.createElement('img')
+    image.setAttribute('src', properties[i].image)
+    card.appendChild(image)
+    propertyContainer.appendChild(card)
+}
+
+
+
+
 
 
 //sorry coach Josh if the file was enormous i had to sort an issue with the compiler(i think?)
